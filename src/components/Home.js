5ec33b77d4dbcacdb31b57a973556097c4e9798e
mike oselle <osellezino@outlook.com>
2020-02-27 12:11:19 +0100

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,71 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      this is home
+
+      <section>
+          <div className="row align-items-center gradient gradient-navy-blue text-contrast">
+            <div className="col-md-6 mx-auto py-4">
+              <blockquote className="bold mt-1 text-contrast"><i className="quote fas fa-quote-left"></i> If your company is struggling to stay in business, there may be several options you can explore. <span className="bold">Dashcore,</span> was the perfect template. It is very easy to implement, has great design, and it has all the functionality we were looking for.</blockquote>
+              <hr className="my-4"/>
+              <div className="small text-contrast"><span className="bold d-block">Jean Doe,</span> CEO & Founder of Awesome Company</div>
+            </div>            
+          </div>
+        </section>
+      <section className="section quick-start-your-business">
+            <div className="container-fluid bring-to-front pb-md-10">
+                <div className="row gap-y align-items-center">
+                    <div className="col-10 col-lg-4 mx-auto order-md-2">
+                        <div className="mb-5 text-center text-lg-left">
+                            <h2>Quick start your business<br/><span className="text-primary">with the right tools</span></h2>
+                            <p className="lead text-muted">DashCore helps you Build a modern & beautiful web presence. Our growing set of components will make your life waaaay easier.</p>
+                        </div>
+                        <ul className="list-unstyled">
+                            <li className="py-2">
+                                <div className="d-flex align-items-center">
+                                    <div className="icon-md bg-primary p-2 rounded-circle center-flex mr-3"><i data-feather="arrow-right" className="stroke-contrast"></i></div>
+                                    <p className="my-0">Perfect for modern and growing Apps &amp; Startups</p>
+                                </div>
+                            </li>
+                            <li className="py-2">
+                                <div className="d-flex align-items-center">
+                                    <div className="icon-md bg-primary p-2 rounded-circle center-flex mr-3"><i data-feather="arrow-right" className="stroke-contrast"></i></div>
+                                    <p className="my-0">Predesigned growing set of modern web components</p>
+                                </div>
+                            </li>
+                            <li className="py-2">
+                                <div className="d-flex align-items-center">
+                                    <div className="icon-md bg-primary p-2 rounded-circle center-flex mr-3"><i data-feather="arrow-right" className="stroke-contrast"></i></div>
+                                    <p class="my-0">Modern &amp; eye-catching design to enchant your visitors</p>
+                                </div>
+                            </li>
+                            <li className="py-2">
+                                <div className="d-flex align-items-center">
+                                    <div className="icon-md bg-primary p-2 rounded-circle center-flex mr-3"><i data-feather="arrow-right" className="stroke-contrast"></i></div>
+                                    <p className="my-0">Focus on your business, don&#39;t worry about your website</p>
+                                </div>
+                            </li>
+                        </ul><a href="#!" className="btn btn-primary btn-rounded mt-4">Know More</a>
+                    </div>
+                    <div className="col-12 col-lg-6 pl-0 order-md-1">
+                        <div className="browser shadow" data-aos="fade-right">
+                            <div className="screen"><img src="img/screens/dash/1.png" alt="" className="img-responsive"/></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+      <section className="singl-testimonial shadow">
+        <div className="container-fluid py-0">
+          <div className="row align-items-center gradient gradient-blue-navy text-contrast">
+            <div className="col-md-4 mx-auto py-4 py-md-0">
+              <p className="font-md bold mt-1 text-contrast">Every child deserves to be taken care of and to have a home and family. Our charity helps those people who have no hope</p>
+              <div className="small text-contrast">You can contribute your share for this Child</div><div><a href="/donate" className="btn btn-primary btn-rounded mt-3">Donate</a></div>
+            </div>
+            <div className="col-12 col-md-6 image-background cover" style={{"background-image": "url(img/testimonials/1.jpg)"}}>
+            </div>
+        </div>
+        </div>
+      </section>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import logo from '../logo.jpg';
 export default function Navbar() {
   return (
-    <div>
-      this is Navbar
-    </div>
+    <nav className="navbar navbar-expand-md navigation sidebar-left block bg-contrast">
+                <div className="container py-4"><button className="navbar-toggler" type="button"><span className="icon-bar"></span> <span className="icon-bar"></span> <span className="icon-bar"></span></button> <a href="#main" className="navbar-brand"><img src={logo} alt="Dashcore" className="logo logo-sticky"/></a>
+                    <div className="collapse navbar-collapse ml-auto">
+                        <div className="sidebar-brand"><a href="#main"><img src={logo} alt="Dashcore Template" className="logo"/></a></div>
+                        <ul className="nav navbar-nav ml-auto">
+                            <li className="nav-item"><a className="nav-link" href="javascript:;">Home</a></li>
+                            <li className="nav-item dropdown mega"><a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#pricing">Custom menu</a>
+                                <div className="dropdown-menu dropdown-menu-right">
+                                    <div className="row">
+                                        <div className="col-md-4 b-r"><a className="dropdown-item text-wrap" target="_blank" href="javascript:;">
+                                                <p className="bold my-0">About</p>
+                                            </a><a className="dropdown-item text-wrap" target="_blank" href="javascript:;">
+                                                <p className="bold my-0">Contact</p>
+                                            </a><a className="dropdown-item text-wrap" target="_blank" href="javascript:;">
+                                                <p className="bold my-0">Blog</p>
+                                            </a></div>
+                                        <div className="col-10 mx-auto col-md-8">
+                                            <h3 className="text-darker mb-0 light mt-4 mt-md-0">Think about<span className="bold d-block">your business</span></h3>
+                                            <p className="text-secondary">Our best bet is you, we can assure that our template will fit the most of your needs.</p><a href="javascript:;" className="btn btn-rounded btn-info mt-3">View plan & prices</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="nav-item"><a className="nav-link" href="javascript:;">Pricing</a></li>
+                            <li className="nav-item"><a className="nav-link" href="javascript:;">Download</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
   )
 }
